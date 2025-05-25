f8490eb8f369d30410a9fa5cf738129995344840
s4009378 <167508316+TheLittlePenguin@users.noreply.github.com>
2025-05-25 05:02:07 +0000

    added 1st condition of addperson

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -16,7 +16,19 @@ private boolean isSuspended;
 private String filePath = "person.txt";
 
 
-public boolean addPerson() {
+public boolean addPerson(String personID) {
+    if (personID.length() != 10) return false;
+
+        
+    if (!Character.isDigit(personID.charAt(0)) || !Character.isDigit(personID.charAt(1))) return false;
+    int firstDigit = Character.getNumericValue(personID.charAt(0));
+    int secondDigit = Character.getNumericValue(personID.charAt(1));
+    if (firstDigit < 2 || firstDigit > 9 || secondDigit < 2 || secondDigit > 9) return false;
+
+       
+    char secondLast = personID.charAt(8);
+    char last = personID.charAt(9);
+    if (!Character.isUpperCase(secondLast) || !Character.isUpperCase(last)) return false;
 
 
     return true;
