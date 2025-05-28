826388b1d9bec0f52c08e18d34a74c4c8b3f42d9
s4009378 <167508316+TheLittlePenguin@users.noreply.github.com>
2025-05-28 04:27:04 +0000

    adding tests

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,3 +1,6 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -5,6 +8,7 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PersonTest {
 
@@ -22,5 +26,38 @@ public class PersonTest {
     public void cleanup() {
         new File(TEST_FILE).delete();
     }
+
+        @Test
+    public void testAddPersonValid() {
+        boolean result = Person.addPerson("56s_d%&fAB", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990", TEST_FILE);
+        assertTrue(result, "Should add valid person");
+    }
+
+    @Test
+    public void testAddPersonInvalidIDFormat() {
+        boolean result = Person.addPerson("12abcdE123", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990", TEST_FILE);
+        assertFalse(result, "ID is not valid format");
+    }
+
+    @Test
+    public void testAddPersonInvalidAddressState() {
+        boolean result = Person.addPerson("56s_d%&fAB", "32|Highland Street|Melbourne|NSW|Australia", "15-11-1990", TEST_FILE);
+        assertFalse(result, "State must be Victoria");
+    }
+
+    @Test
+    public void testAddPersonInvalidBirthdateFormat() {
+        boolean result = Person.addPerson("56s_d%&fAB", "32|Highland Street|Melbourne|Victoria|Australia", "1990-11-15", TEST_FILE);
+        assertFalse(result, "Birthdate format must be DD-MM-YYYY");
+    }
+
+    @Test
+    public void testAddPersonMissingSpecialCharactersInID() {
+        boolean result = Person.addPerson("56abcdefAB", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990", TEST_FILE);
+        assertFalse(result, "ID must have at least two special characters");
+    }
+
+
+
     
 }
